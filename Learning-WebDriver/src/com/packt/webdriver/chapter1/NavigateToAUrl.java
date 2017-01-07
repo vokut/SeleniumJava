d363664b761527cfe61d300b97936dad4f0b5031
vokut <ttukov@gmail.com>
2017-01-07 22:37:28 +0200

    driver.quit() added

--- Learning-WebDriver/src/com/packt/webdriver/chapter1/NavigateToAUrl.java
+++ Learning-WebDriver/src/com/packt/webdriver/chapter1/NavigateToAUrl.java
@@ -2,11 +2,13 @@ package com.packt.webdriver.chapter1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class NavigateToAUrl {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver","D:\\Selenium\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
+		driver.quit();
 	}
 }
