a200119b263d7a50a6f06427063f3aa9a3250ac0
vokut <ttukov@gmail.com>
2017-01-07 22:05:13 +0200

    NavigateToAUrl

---
+++ Learning-WebDriver/src/com/packt/webdriver/chapter1/NavigateToAUrl.java
@@ -0,0 +1,12 @@
+package com.packt.webdriver.chapter1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigateToAUrl {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","D:\\Selenium\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+	}
+}
